69a4677fb52cf319b8ccee9c644f11719f4d9867
Andre Tischenko <39458631+Fuegos@users.noreply.github.com>
2022-10-12 13:43:52 +0400

    complite without optimisation

--- StringToParentheses/StringToParentheses.cpp
+++ StringToParentheses/StringToParentheses.cpp
@@ -1,20 +1,33 @@
-// StringToParentheses.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string inputStr{};
+    char isAgain{'Y'};
+    while (isAgain == 'Y') 
+    {
+        cout << "Your text for replaced: ";
+        getline(cin >> ws, inputStr);
+        string resultStr{ inputStr };
+        const int sizeInputStr{ static_cast<int>(inputStr.length()) };
+        for (int i{ 0 }; i < sizeInputStr; ++i) {
+            bool isDuplicate{ false };
+            for (int j{ 0 }; j < sizeInputStr; ++j) {
+                if (i != j) {
+                    if (tolower(inputStr[i]) == tolower(inputStr[j])) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+            }
+            resultStr[i] = isDuplicate ? ')' : '(';
+        }
+        cout << "Result: " << inputStr << " => " << resultStr << '\n';
+        cout << "Again? ";
+        cin >> isAgain;
+    }
+    
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
