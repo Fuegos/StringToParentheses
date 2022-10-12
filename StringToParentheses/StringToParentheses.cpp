cd03b715659e755a8630beeba390b85ff9999c4a
Andre Tischenko <39458631+Fuegos@users.noreply.github.com>
2022-10-12 22:03:32 +0400

    optimisation with map
    change complexity from O(n*n) to O(n*log n)

--- StringToParentheses/StringToParentheses.cpp
+++ StringToParentheses/StringToParentheses.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
 #include <string>
+#include <map>
+
 using namespace std;
 
 int main()
 {
     string inputStr{};
     char isAgain{'Y'};
+
     while (isAgain == 'Y') 
     {
         cout << "Your text for replaced: ";
         getline(cin >> ws, inputStr);
-        string resultStr{ inputStr };
+
+        // for storing pair: symbol of input string and its index
+        map<char, int> symbolIndexes;
         const int sizeInputStr{ static_cast<int>(inputStr.length()) };
+        string resultStr{ inputStr };
+
         for (int i{ 0 }; i < sizeInputStr; ++i) {
-            bool isDuplicate{ false };
-            for (int j{ 0 }; j < sizeInputStr; ++j) {
-                if (i != j) {
-                    if (tolower(inputStr[i]) == tolower(inputStr[j])) {
-                        isDuplicate = true;
-                        break;
-                    }
-                }
+            // replace each symbol as unique
+            resultStr[i] = '(';
+            char symbol{ (char) tolower(inputStr[i]) };
+            if (symbolIndexes.find(symbol) != symbolIndexes.end()) {
+                // make changing '(' => ')' after next appearing of symbol
+                resultStr[i] = ')';
+                resultStr[symbolIndexes[symbol]] = ')';
             }
-            resultStr[i] = isDuplicate ? ')' : '(';
+            else {
+                // store first appearing symbol
+                symbolIndexes.insert({ symbol, i });
+            }
         }
+        
         cout << "Result: " << inputStr << " => " << resultStr << '\n';
-        cout << "Again? ";
-        cin >> isAgain;
+        isAgain = '\0';
+        while (isAgain != 'Y' && isAgain != 'N')
+        {
+            cout << "Again? (Y/N) ";
+            cin >> isAgain;
+        }
     }
     
     return 0;
